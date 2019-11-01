01a385c49dee084a74dc5ceaf65377323f7ee4d5
hunter <328624847@qq.com>
2019-11-01 13:42:38 +0800

    原始文件修改

--- flasky.py
+++ flasky.py
@@ -1,26 +1,31 @@
-import os
+# -*- coding: UTF-8 -*-
+__author__ = 'hunter'
 import click
 from flask_migrate import Migrate
 from app import create_app, db
-from app.models import User, Role
+from app.models.user import User, Role
 
 # app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 app = create_app()
 migrate = Migrate(app, db)
 
 
-@app.shell_context_processor
-def make_shell_context():
-    return dict(db=db, User=User, Role=Role)
+# @app.shell_context_processor
+# def make_shell_context():
+#     return dict(db=db, User=User, Role=Role)
+#
+#
+# @app.cli.command()
+# @click.argument('test_names', nargs=-1)
+# def test(test_names):
+#     """Run the unit tests."""
+#     import unittest
+#     if test_names:
+#         tests = unittest.TestLoader().loadTestsFromNames(test_names)
+#     else:
+#         tests = unittest.TestLoader().discover('tests')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
 
 
-@app.cli.command()
-@click.argument('test_names', nargs=-1)
-def test(test_names):
-    """Run the unit tests."""
-    import unittest
-    if test_names:
-        tests = unittest.TestLoader().loadTestsFromNames(test_names)
-    else:
-        tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+if __name__ == '__main__':
+    app.run()
