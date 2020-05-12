f396cd4f95246f5ccda7ee1fbc0e4ebe7f0b4661
hunter <328624847@qq.com>
2020-05-12 14:03:34 +0800

    html渲染提供示例

--- app/main/views.py
+++ app/main/views.py
@@ -32,6 +32,26 @@ def index():
                            known=session.get('known', False))
 
 
+@main.route('/home', methods=['GET', 'POST'])
+def home():
+    # form = NameForm()
+    # if form.validate_on_submit():
+    #     user = User.query.filter_by(username=form.name.data).first()
+    #     if user is None:
+    #         user = User(username=form.name.data)
+    #         db.session.add(user)
+    #         db.session.commit()
+    #         session['known'] = False
+    #         if current_app.config['FLASKY_ADMIN']:
+    #             send_email(current_app.config['FLASKY_ADMIN'], 'New User',
+    #                        'mail/new_user', user=user)
+    #     else:
+    #         session['known'] = True
+    #     session['name'] = form.name.data
+    #     return redirect(url_for('.index'))
+    return render_template("home.html")
+
+
 @main.route("/health")
 def health():
     print(2333)
